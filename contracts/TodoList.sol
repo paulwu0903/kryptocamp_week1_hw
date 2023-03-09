3754920d63042d4babf72b8c961d185da86810d9
Paul Wu <paulwu0903@gmail.com>
2023-03-09 23:04:24 +0800

    Modify the struct

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,132 +3,174 @@ pragma solidity ^0.8.4;
 
 contract TodoList {
 
+    enum State{
+        todo,
+        pending,
+        completed
+    }
+
 
     struct Item{
         string content;
+        State state;
         uint256 pendingStartTime;
     }
 
 
-    Item[] public todos;
-    Item[] public todoCompleted;
-    Item[] public pending;
+    mapping (uint256 => Item) todoMap;
+    
 
     uint256 public pendingThreshold; 
+
+    uint256 public counter = 0;
 
     constructor(uint256 _pendingLimit) {
         pendingThreshold = _pendingLimit;
     }
 
+    modifier isTodo(uint256 _id){
+        require(todoMap[_id].state == State.todo, "Its state is not \"todo\"");
+        _;
+    }
+
+    modifier isPending(uint256 _id){
+        require(todoMap[_id].state == State.pending, "Its state is not \"pending\"");
+        _;
+    }
+
+    modifier isCompleted(uint256 _id){
+        require(todoMap[_id].state == State.completed, "Its state is not \"completed\"");
+        _;
+    }
+    
     modifier overRecoverTime(uint256 pendingStartTime){
         require(block.timestamp - pendingStartTime <= pendingThreshold, "over time !");
         _;
     }
 
+
     // 永久儲存是storage，短暫使用就用memory，calldata傳入後只能參考不能改變的data type 
-    function addTodo(string memory todo) external {
+    function addTodo(string memory _content) external {
         Item memory todoItem = Item({
-            content: todo,
-            pendingStartTime: 0 
+            content: _content,
+            pendingStartTime: 0,
+            state: State.todo 
             });
-        todos.push(todoItem);
+        todoMap[counter] = todoItem;
+        counter++;
     }
 
-    function setCompleted(uint256 index) external {
-
-        Item memory compeltedTodo = pending[index];
-
-        for (uint256 i = index; i < pending.length - 1; i++){
-            pending[i] = pending[i + 1];
-        }
-
-        delete pending[pending.length - 1];
-
-        pending.pop();
-
-        todoCompleted.push(compeltedTodo);
+    function setCompleted(uint256 _id) external isPending(_id){
+        Item memory todo = todoMap[_id];
+        todo.state= State.completed;
+        todoMap[_id] = todo;
     }
 
-    function getTodo(uint256 index) external view returns (string memory) {
-        return todos[index].content;
+    function deleteTodo(uint256 _id) external {
+        delete todoMap[_id];    
     }
 
-    function deleteTodo(uint256 index) external {
-
-        delete todos[index];
-
-        //調整刪除arr元素，自動排序arr內容，並pop空間 (如此會增加deleteTodo所需支付的Gas Fee)
-        for(uint256 i = index; i < todos.length - 1 ; i++){
-            todos[i] = todos[i+1];
-        }
-
-        todos.pop();
+    function getTodo(uint256 _id) external isTodo(_id) view returns (string memory) {
+        return todoMap[_id].content;
     }
 
-    function getCompleted(uint256 index) external view returns (string memory) {
-        return todoCompleted[index].content;
+    function getPending (uint256 _id) external isPending(_id) view returns (string memory){
+        return todoMap[_id].content;
+    }
+
+    function getCompleted(uint256 _id) external isCompleted(_id) view returns (string memory) {
+        return todoMap[_id].content;
     }
 
     function getAllTodo() external view returns (string[] memory) {
-        string[] memory res = new string[](todos.length);
-        for (uint256 i =0; i< todos.length; i++){
-            res[i] = todos[i].content;
+        uint256 arrSize = getTodoNumbers();
+        uint256 index = 0;
+        string[] memory res = new string[](arrSize) ;
+        for (uint256 i =0; i< counter; i++){
+            if (todoMap[i].state == State.todo){
+                res[index] = todoMap[i].content;
+                index++;
+            }
         }
         return res;
-    }
-
-    function getAllCompleted() external view returns (string[] memory) {
-        string[] memory res = new string[](todoCompleted.length);
-        for (uint256 i =0; i< todoCompleted.length; i++){
-            res[i] = todoCompleted[i].content;
-        }
-        return res;
-    }
-
-    function setPending(uint256 _index) external {
-        //取得要完成todo list 事項的內容
-        Item memory pendingTodo = todos[_index];
-        pendingTodo.pendingStartTime = block.timestamp;
-        //重新整理arr內容，將完成事項後面的內容網前移動
-        for (uint256 i = _index; i < todos.length - 1; i++){
-            todos[i] = todos[i + 1];
-        }
-        //將最後一項還原成預設直
-        delete todos[todos.length - 1];
-        //將arr最後一個剛被還原的空間刪掉(arr長度-1)
-        todos.pop();
-
-        //將完成事項加入另一個list中
-        pending.push(pendingTodo);
-    } 
-
-    function getPending (uint256 _index) external view returns (string memory){
-        return pending[_index].content;
     }
 
     function getAllPending()external view returns(string[] memory) {
-        string[] memory res = new string[](pending.length);
-        for (uint256 i =0; i< pending.length; i++){
-            res[i] = pending[i].content;
+        uint256 arrSize = getPendingNumbers();
+        uint256 index = 0;
+        string[] memory res = new string[](arrSize) ;
+        for (uint256 i =0; i< counter; i++){
+            if (todoMap[i].state == State.pending){
+                res[index] = todoMap[i].content;
+                index++;
+            }
         }
         return res;
     }
 
-    function recoverFromPendingToTodo(uint256 _index) external overRecoverTime(pending[_index].pendingStartTime) {
-        Item memory pendingRecoverItem = pending[_index];
-        todos.push(pendingRecoverItem);
-
-        for(uint256 i= _index; i < pending.length-1; i++){
-            pending[i] = pending[i+1];
+    function getAllCompleted() external view returns(string[] memory) {
+        uint256 arrSize = getCompletedNumbers();
+        uint256 index = 0;
+        string[] memory res = new string[](arrSize) ;
+        for (uint256 i =0; i< counter; i++){
+            if (todoMap[i].state == State.completed){
+                res[index] = todoMap[i].content;
+                index++;
+            }
         }
-        delete pending[pending.length-1];
-        pending.pop();
+        return res;
+    }
+
+    function getTodoNumbers() private view returns (uint256){
+        uint256 count = 0;
+         for (uint256 i =0; i< counter; i++){
+            if (todoMap[i].state == State.todo){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    function getPendingNumbers() private view returns (uint256){
+        uint256 count = 0;
+         for (uint256 i =0; i< counter; i++){
+            if (todoMap[i].state == State.pending){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    function getCompletedNumbers() private view returns (uint256){
+        uint256 count = 0;
+         for (uint256 i =0; i< counter; i++){
+            if (todoMap[i].state == State.completed){
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    function setPending(uint256 _id) external isTodo(_id) {
+        Item memory todo = todoMap[_id];
+        todo.state= State.pending;
+        todo.pendingStartTime = block.timestamp;
+        todoMap[_id] = todo;
+    } 
+
+    function recoverFromPendingToTodo(uint256 _id) external isPending(_id) overRecoverTime(todoMap[_id].pendingStartTime) {
+        Item memory todo = todoMap[_id];
+        todo.state = State.todo;
+        todo.pendingStartTime = 0;
+        todoMap[_id] = todo;
     }
 
     function cleanCompleted() external {
-        delete todoCompleted;
-        for(uint256 i=0; i< todoCompleted.length; i++){
-            todoCompleted.pop();
+        for (uint256 i =0; i< counter; i++){
+            if (todoMap[i].state == State.completed){
+                delete todoMap[i];
+            }
         }
     }
 }
